333bdd365c42a433b51b54e2a691514dbb6664ad
tuvafalk <tuva.falk@gmail.com>
2023-03-03 11:52:10 +0100

    Print and some more

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("drop").classList.toggle("show");
+
+    var x = document.getElementById("drop");
+
+    if (x.className === "dropdown-content" ) {
+      x.className += " show";
+    } else {
+      x.className = "dropdown-content";
+    }
+}
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+}
+
 // Change menu to responsive
 function topnav_resp() {
     var x = document.getElementById("myMenu");
-    
+
     if (x.className === "menu" ) {
       x.className += " responsive";
     } else {
